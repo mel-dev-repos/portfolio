4fe9ace27755d1f27f95e480afc303be7271b4cf
Melina <musakhani1379@gmail.com>
2025-03-25 00:32:32 +0330

    set link for header titles and set position of header fixed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   const header_menu = [
     {
       title: "About me",
-      link: "",
+      link: "#about-me",
     },
     {
       title: "Skills & Tools",
@@ -20,14 +20,17 @@ export default function Header() {
     },
   ];
   return (
-    <div className="flex text-white justify-between items-center">
-      <h1>Melina Musakhani</h1>
+    <div className="flex bg-[#1E1E2F] w-full fixed top-0 text-white justify-between p-5 items-center">
+      <h1>Melina Musakhani Portfolio</h1>
       <div className="flex gap-4 items-center">
         {header_menu.map(({ title, link }, index) => {
           return (
-            <a key={index} href={link}>
-              {title}
-            </a>
+            <div key={index}>
+              <a className="relative inline-block group" href={link}>
+                {title}
+                <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-gradient-to-r from-purple-400 to-[#5c8ce5] group-hover:w-full transition-all duration-250"></span>
+              </a>
+            </div>
           );
         })}
       </div>
