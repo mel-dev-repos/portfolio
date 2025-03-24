17ff89ed73aa76460890656cc63d6dbb66c8550c
Melina <musakhani1379@gmail.com>
2025-03-25 00:31:38 +0330

    import about me component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "@/components/about-me";
+import HomeComponent from "@/components/about-me";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-    
+    <div className="mt-28  px-10">
+    <AboutMe />
     </div>
   );
 }
