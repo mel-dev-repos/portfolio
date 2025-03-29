3b478ac0b496ead54a13cc9aee3a993c991abef4
Melina <musakhani1379@gmail.com>
2025-03-29 11:32:11 +0330

    handle skills href tag

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -4,7 +4,7 @@ import Tools from "./components/toolsx";
 
 export default function Skills() {
   return (
-    <div>
+    <div id="skills">
       <h2 className="text-[2rem] text-white">Skills & Tools</h2>
       <div className="flex justify-center items-center gap-20">
         <GradientBoxes title="Programming Languages" />
