beb66fb3ab2dc051d9fd76932534fe7a1128cb68
Melina <musakhani1379@gmail.com>
2025-03-29 11:32:05 +0330

    import soft skills component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "@/components/about-me";
 import Skills from "@/components/skills";
+import SoftSkills from "@/components/soft-skills";
 
 export default function Home() {
   return (
     <div className="mt-28  px-10">
     <AboutMe />
     <Skills />
+    <SoftSkills />
     </div>
   );
 }
